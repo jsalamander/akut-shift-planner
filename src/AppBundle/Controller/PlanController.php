4465811d6f1f024e7c26a63ce1b4eeec33cf0374
Fribim <jan.friedli@gmx.ch>
2017-06-06 21:12:19 +0200

    generated plan crud

---
+++ src/AppBundle/Controller/PlanController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Plan;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Plan controller.
+ *
+ * @Route("plan")
+ */
+class PlanController extends Controller
+{
+    /**
+     * Lists all plan entities.
+     *
+     * @Route("/", name="plan_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $plans = $em->getRepository('AppBundle:Plan')->findAll();
+
+        return $this->render('plan/index.html.twig', array(
+            'plans' => $plans,
+        ));
+    }
+
+    /**
+     * Creates a new plan entity.
+     *
+     * @Route("/new", name="plan_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $plan = new Plan();
+        $form = $this->createForm('AppBundle\Form\PlanType', $plan);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($plan);
+            $em->flush();
+
+            return $this->redirectToRoute('plan_show', array('id' => $plan->getId()));
+        }
+
+        return $this->render('plan/new.html.twig', array(
+            'plan' => $plan,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a plan entity.
+     *
+     * @Route("/{id}", name="plan_show")
+     * @Method("GET")
+     */
+    public function showAction(Plan $plan)
+    {
+        $deleteForm = $this->createDeleteForm($plan);
+
+        return $this->render('plan/show.html.twig', array(
+            'plan' => $plan,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing plan entity.
+     *
+     * @Route("/{id}/edit", name="plan_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Plan $plan)
+    {
+        $deleteForm = $this->createDeleteForm($plan);
+        $editForm = $this->createForm('AppBundle\Form\PlanType', $plan);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('plan_edit', array('id' => $plan->getId()));
+        }
+
+        return $this->render('plan/edit.html.twig', array(
+            'plan' => $plan,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a plan entity.
+     *
+     * @Route("/{id}", name="plan_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Plan $plan)
+    {
+        $form = $this->createDeleteForm($plan);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($plan);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('plan_index');
+    }
+
+    /**
+     * Creates a form to delete a plan entity.
+     *
+     * @param Plan $plan The plan entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Plan $plan)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('plan_delete', array('id' => $plan->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
