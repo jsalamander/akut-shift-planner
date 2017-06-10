c234f7775e7a5b8754759d303be0f643ff2fed4d
Fribim <jan.friedli@gmx.ch>
2017-06-11 00:40:56 +0200

    finish recover password

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -35,7 +36,7 @@ class DefaultController extends Controller
     /**
      * @Route("/lost-plan-pass", name="recover_plan_password")
      */
-    public function lostPlanPasswordAction(Request $request)
+    public function lostPlanPasswordAction(Request $request, \Swift_Mailer $mailer)
     {
         $em = $this->getDoctrine()->getManager();
 
@@ -43,19 +44,59 @@ class DefaultController extends Controller
         $form->handleRequest($request);
 
         $plan = $em->getRepository('AppBundle:Plan')->find($request->get('plan'));
+        $success = false;
+        if($plan->getEmail() !== $form->getData()['email']) {
+            $form->addError(new FormError('Wrong Email'));
+        }
 
         if ($form->isSubmitted() && $form->isValid()) {
-
-            if($plan->getEmail() === $form->getData()['email']) {
-                die;
-            }
+            $this->sendPlanPasswordViaMail($mailer, $plan, $plan->getPassword());
+            $success = true;
         }
 
         return $this->render('default/lost-plan-password.html.twig',
             array(
-                'form' => $form->createView()
+                'success' => $success,
+                'form' => $form->createView(),
+                'plan' => $plan
             )
         );
+    }
+
+    /**
+     * Send the plan password via email to the creator so they
+     * can access the plan later
+     *
+     * @param \Swift_Mailer $mailer
+     * @param $password
+     */
+    private function sendPlanPasswordViaMail(\Swift_Mailer $mailer, $plan, $password) {
+        if (!$plan->getIsTemplate()) {
+            $message = new \Swift_Message('Password Recovery');
+
+            $message->setFrom('no-reply@schicht-plan.ch')
+                ->setTo($plan->getEmail())
+                ->setBody(
+                    $this->renderView(
+                        'email/plan-recover-password.html.twig',
+                        array(
+                            'password' => $password,
+                            'plan_id' => $plan->getId()
+                        )
+                    ),
+                    'text/html'
+                )->addPart(
+                    $this->renderView(
+                        'email/plan-recover-password.txt.twig',
+                        array(
+                            'password' => $password,
+                            'plan_id' => $plan->getId()
+                        )
+                    ),
+                    'text/plain'
+                );
+            $mailer->send($message);
+        }
     }
 
 }
