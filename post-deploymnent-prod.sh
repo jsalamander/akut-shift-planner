bafb7aa51507d50d907db500684822806fa5e8c8
Fribim <jan.friedli@gmx.ch>
2017-06-13 22:06:37 +0200

    added post deployment script for prod instances

--- post-deploymnent-prod.sh
+++ post-deploymnent-prod.sh
@@ -4,6 +4,5 @@ SYMFONY_ENV=prod ./composer.phar install --no-dev --optimize-autoloader
 php bin/symfony_requirements
 php bin/console cache:clear --env=prod --no-debug --no-warmup
 php bin/console cache:warmup --env=prod
-php bin/console doctrine:database:drop --force --env=prod
-php bin/console doctrine:database:create --env=prod
+php bin/console assetic:dump --env=prod --no-debug --env=prod
 php bin/console doctrine:schema:update --force --env=prod
