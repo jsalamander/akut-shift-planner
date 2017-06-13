5ff98025145144eea080dffc1a42926b1965761e
Fribim <jan.friedli@gmx.ch>
2017-06-13 14:37:30 +0200

    added post deployment script

---
+++ post-deployment.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+curl -s https://getcomposer.org/installer | php
+SYMFONY_ENV=prod ./composer.phar install --no-dev --optimize-autoloader
+php bin/symfony_requirements
+php bin/console cache:clear --env=prod --no-debug --no-warmup
+php bin/console cache:warmup --env=prod
+php bin/console doctrine:database:drop --force --env=prod
+php bin/console doctrine:database:create --env=prod
+php bin/console doctrine:schema:update --force --env=prod
