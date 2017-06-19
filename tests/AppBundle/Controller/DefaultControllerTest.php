44be5f61fb8013d675176359e01a97d81e0dc768
Fribim <jan.friedli@gmx.ch>
2017-06-19 16:25:12 +0200

    added first tests

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -9,10 +9,16 @@ class DefaultControllerTest extends WebTestCase
     public function testIndex()
     {
         $client = static::createClient();
-
         $crawler = $client->request('GET', '/');
-
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertContains('Schicht-Plan.ch', $crawler->filter('.jumbotron')->text());
+        $this->assertEquals(1, $crawler->filter('.jumbotron .btn')->count());
+        $this->assertContains('/plan/new', $crawler->filter('.jumbotron .btn')->attr('href'));
+    }
+
+    public function testNavbar() {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+        $this->assertEquals(4, $crawler->filter('.navbar-collapse li')->count());
     }
 }
