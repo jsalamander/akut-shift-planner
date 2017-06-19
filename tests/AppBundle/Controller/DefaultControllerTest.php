44e687a557825ce66c51fbcbdfd29d9d47103c76
Fribim <jan.friedli@gmx.ch>
2017-06-19 23:45:45 +0200

    added basic footer test

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -20,5 +20,14 @@ class DefaultControllerTest extends WebTestCase
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
         $this->assertEquals(4, $crawler->filter('.navbar-collapse li')->count());
+
+    }
+
+    public function testFooter() {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+        $this->assertEquals(2, $crawler->filter('.footer span')->count());
+        $this->assertContains('mailto:jan.friedli@gmx.ch', $crawler->filter('.footer span a')->attr('href'));
+        $this->assertContains('https://github.com/fribim/akut-shift-planner', $crawler->filter('.footer span a')->eq(1)->attr('href'));
     }
 }
