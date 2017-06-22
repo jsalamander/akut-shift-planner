22995497f71f8ebdf6a30553b58f9a97bbb4c35d
Fribim <jan.friedli@gmx.ch>
2017-06-22 12:13:42 +0200

    added test for authenticated plan creation

---
+++ src/AppBundle/DataFixtures/ORM/LoadTemplateData.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\User;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+
+class LoadUserData extends AbstractFixture implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $admin = new User();
+        $admin->setUsername('admin');
+        $admin->setEmail('admin@admin.ch');
+        $admin->setPlainPassword('12345678');
+        $admin->setEnabled(1);
+        $this->setReference('admin-user', $admin);
+        $manager->persist($admin);
+        $manager->flush();
+    }
+}
