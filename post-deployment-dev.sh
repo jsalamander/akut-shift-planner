ea063904068a131fbc0213b3b072815c43c22a72
Fribim <jan.friedli@gmx.ch>
2017-06-26 02:10:14 +0200

    changes post deploy script

--- post-deployment-dev.sh
+++ post-deployment-dev.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-curl -s https://getcomposer.org/installer | php71
+curl -s https://getcomposer.org/installer | php
 SYMFONY_ENV=prod php71 composer.phar install --no-dev --optimize-autoloader
-php71 bin/symfony_requirements
-php71 bin/console cache:clear --env=prod --no-debug --no-warmup
-php71 bin/console cache:warmup --env=prod
-php71 bin/console assetic:dump --env=prod --no-debug --env=prod
-php71 bin/console doctrine:database:drop --force --env=prod
-php71 bin/console doctrine:database:create --env=prod
-php71 bin/console doctrine:schema:update --force --env=prod
+php71 ./bin/symfony_requirements
+php71 ./bin/console cache:clear --env=prod --no-debug --no-warmup
+php71 ./bin/console cache:warmup --env=prod
+php71 ./bin/console assetic:dump --env=prod --no-debug --env=prod
+php71 ./bin/console doctrine:database:drop --force --env=prod
+php71 ./bin/console doctrine:database:create --env=prod
+php71 ./bin/console doctrine:schema:update --force --env=prod
