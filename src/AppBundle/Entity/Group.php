fe678946409d3da8cbd770f2edb96905b03f9847
Fribim <jan.friedli@gmx.ch>
2017-06-26 13:29:59 +0200

    init groups

---
+++ src/AppBundle/Entity/Group.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\Group as BaseGroup;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_group")
+ */
+class Group extends BaseGroup
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+}
