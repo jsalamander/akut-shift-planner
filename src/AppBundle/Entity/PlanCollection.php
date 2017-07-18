95246a5d960bbe11ddc9a19f5be1b316dffb4183
Fribim <jan.friedli@gmx.ch>
2017-07-18 14:27:27 +0200

    generated plan  collection

---
+++ src/AppBundle/Entity/PlanCollection.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * PlanCollection
+ *
+ * @ORM\Table(name="plan_collection")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PlanCollectionRepository")
+ */
+class PlanCollection
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return PlanCollection
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+}
+
