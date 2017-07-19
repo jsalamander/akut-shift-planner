f01e8271c876c4c8c41a41220a795e0324407db0
Fribim <jan.friedli@gmx.ch>
2017-07-19 14:34:16 +0200

    added no title tetst

--- tests/AppBundle/Controller/PlanCollectionControllerTest.php
+++ tests/AppBundle/Controller/PlanCollectionControllerTest.php
@@ -10,13 +10,15 @@ class PlanCollectionControllerTest extends WebTestCase
 
     private $client;
 
+    private $fixtures;
+
     public function setUp()
     {
-        $fixtures = $this->loadFixtures(array(
+        $this->fixtures = $this->loadFixtures(array(
             'AppBundle\DataFixtures\ORM\LoadCompleteDataSet'
         ))->getReferenceRepository();
 
-        $this->loginAs($fixtures->getReference('admin-user'), 'main');
+        $this->loginAs($this->fixtures->getReference('admin-user'), 'main');
         $this->client = $this->makeClient();
         $this->client->request('GET', '/plancollection');
         $this->assertEquals(301, $this->client->getResponse()->getStatusCode());
@@ -30,9 +32,42 @@ class PlanCollectionControllerTest extends WebTestCase
         $this->assertContains('Warnung! Keine Plan Sammlung erstellt', $this->crawler->filter('.alert-warning')->text());
     }
 
-    public function createACollection()
+    public function testCreateACollection()
     {
-
+        $link = $this->crawler->filter('.btn')->link();
+        $this->crawler = $this->client->click($link);
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        $this->assertContains('Neue Plansammlung', $this->crawler->filter('h1')->text());
+        $this->createACollection();
+        $this->assertContains('admin plan', $this->crawler->filter('tbody')->text());
+        $this->assertContains(
+            'http://localhost/plan/'.$this->fixtures->getReference('admin-plan')->getId(),
+            $this->crawler->filter('tbody')->text()
+        );
     }
 
+    public function testNoTitle () {
+        $this->crawler = $this->client->request('GET', '/plancollection/new');
+        $adminPlanId = $this->fixtures->getReference('admin-plan')->getId();
+        $form = $this->crawler->filter('.btn-primary')->form(array(
+            'appbundle_plancollection[title]' => '',
+            'appbundle_plancollection[plans]' => array($adminPlanId)
+        ));
+
+        $this->crawler = $this->client->submit($form);
+        $this->assertEquals(1, $this->crawler->filter('.alert')->count());
+    }
+
+    public function createACollection()
+    {
+        $adminPlanId = $this->fixtures->getReference('admin-plan')->getId();
+        $form = $this->crawler->filter('.btn-primary')->form(array(
+            'appbundle_plancollection[title]' => 'test collection',
+            'appbundle_plancollection[plans]' => array($adminPlanId)
+        ));
+
+        $this->crawler = $this->client->submit($form);
+        $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
+        $this->crawler = $this->client->followRedirect();
+    }
 }
