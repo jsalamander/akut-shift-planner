442e7352c08d9f3e1ca01da58c0badca2436b410
Fribim <jan.friedli@gmx.ch>
2017-07-25 14:41:40 +0200

    added sharing buttons

--- src/AppBundle/Resources/public/js/global.js
+++ src/AppBundle/Resources/public/js/global.js
@@ -2,4 +2,7 @@ $(function() {
     //init scrolling animations
     new WOW().init();
     $('.multiselect').multiselect();
+    SocialShareKit.init({
+        text: "Schreibe dich ein!"
+    });
 });
