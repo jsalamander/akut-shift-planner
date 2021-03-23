6df95fa9ce15d43161ff866bd85fd4c47e390b97
Jfriedli <jan.friedli@immerda.ch>
2021-03-23 16:08:55 +0100

    almost working

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
